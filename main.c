f87dde1ad57846e6bc899a3012cdabb901fd8272
Gabriel Vieira <gabrielvieira1@protonmail.com>
2022-09-01 01:56:48 -0300

    Exercicios hacker rank

--- main.c
+++ main.c
@@ -36,5 +36,20 @@ int main(int argc, char const *argv[])
 
  free(chutes);
 
+ // Exercicio
+
+ char ch;
+ char s[10];
+ char sen[50];
+
+ scanf("%c", &ch);
+ scanf("%s", s);
+ scanf("\n");
+ scanf("%[^\n]%*c", sen);
+
+ printf("%c\n", ch);
+ printf("%s\n", s);
+ printf("%s\n", sen);
+
  return 0;
 }
